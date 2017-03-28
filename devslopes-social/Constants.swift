203dd417e6813b8395be15838910f48f10382777
Shin Park <shinpark@shins-mac-mini.home>
2017-03-28 11:38:23 -0500

    Sign in ui custom classes complete

---
+++ devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  devslopes-social
+//
+//  Created by Shin Park on 3/28/17.
+//  Copyright © 2017 shinDev. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
