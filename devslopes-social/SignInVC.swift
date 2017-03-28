586111a58f6d4a1c27139a8e8de6d0b84c3ee6b4
Shin Park <shinpark@Shins-Mac-mini.home>
2017-03-28 00:08:22 -0500

    Completed setting up Firebase integration

---
+++ devslopes-social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  devslopes-social
+//
+//  Created by Shin Park on 3/27/17.
+//  Copyright © 2017 shinDev. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
