d42b77699a38f91956d0f234b0c08f58fd9f24c2
Shin Park <shinpark@shins-mac-mini.home>
2017-03-28 22:19:19 -0500

    Social Network - UI for social feed

--- devslopes-social/CircleView.swift
+++ devslopes-social/CircleView.swift
@@ -1,5 +1,5 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  devslopes-social
 //
 //  Created by Shin Park on 3/28/17.
@@ -8,17 +8,19 @@
 
 import UIKit
 
-class FancyView: UIView {
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
-        
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
     }
 
 }
