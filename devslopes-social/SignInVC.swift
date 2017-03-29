134ec68ab57cbcac06c75b3c7ca45429550c1c53
Shin Park <shinpark@shins-mac-mini.home>
2017-03-29 09:25:54 -0500

    Social Network - Creating database users for Firebase

--- devslopes-social/SignInVC.swift
+++ devslopes-social/SignInVC.swift
@@ -53,7 +53,8 @@ class SignInVC: UIViewController {
             } else {
                 print("SHIN: Successfully authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
         })
@@ -66,7 +67,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("SHIN: Email user authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
@@ -75,7 +77,8 @@ class SignInVC: UIViewController {
                         } else {
                             print("SHIN: Successfully authenticated with Firebase")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                         }
                     })
@@ -84,7 +87,9 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("SHIN: Data saved to keychain \(keychainResult)")
